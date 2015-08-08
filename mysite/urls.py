d0d4f3e2d428599d44bed2a79eb2b42aa08ee9d7
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2015-08-08 14:25:30 -0300

    Parte 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+# Django compara la url del request con todas las regexp desde la 
+# primera a la ultima (el orden importa).
+# Cuando machea la url, invoca el view y le pasa como primer arg
+# un objeto HttpRequest y si la regexp tiene algun valor "capturado",
+# tambien se lo pasa, el valor capturado puede ser posicional o "nombrado".
+# El include incluye (valga la redundancia) las urls definidas en la app
+# polls. El namespace es util para reverse url lookup en el caso de que se
+# tengan muchas apps y pueda existir colisiones de nombres de urls. ver
+# /polls/templates/polls/index.html
 
+urlpatterns = [
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Question
+
+# Las view son las encargadas de responder (con un `HttpResponse`) los
+# request (`HttpRequest`). La view que se "elige" se resuelve en urls.py
+# (`django.core.urlresolvers`)
+# Una view tiene que retornanr un `HttpResponse` o levantar una excepcion
+#
+# Django provee una serie de shortcuts para acciones comunes
+#
+# render
+# ======
+#
+# ```python
+# context = {'latest_question_list': latest_question_list}
+# return render(request, 'polls/index.html', context)
+# ```
+#
+# es equivalente
+#
+# ```python
+# template = loader.get_template('polls/index.html')
+# context = RequestContext(request, {
+#     'latest_question_list': latest_question_list,
+# })
+# return HttpResponse(template.render(context))
+# ```
+# get_object_or_404
+# =================
+#
+# `question = get_object_or_404(Question, pk=question_id`
+#
+# es equivalente
+#
+# ```python
+# try:
+#     question = Question.objects.get(pk=question_id)
+# except Question.DoesNotExist:
+#     raise Http404('question does not exist')
+# ```
+#
+# tambien existe `get_list_or_404` que funcion de la misma manera pero
+# con `filter` en lugar de `get`
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
